fbf1c246af8babe169fc289cef40405eaf2f1651
August Biadala <augustbiadala@gmail.com>
2022-05-01 11:19:53 +0200

    Fix secret key leak - mongo db uri, creat .envexample in frontend part

--- backend/src/helpers/clearDatabase.js
+++ backend/src/helpers/clearDatabase.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const Task = require('../models/task');
 const Client = require('../models/client');
 
 const conn = async () => {
-  await mongoose.connect(
-    'mongodb+srv://admin:kinga1992@cluster0.4ww5x.mongodb.net/crm-photo-test?retryWrites=true&w=majority'
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 const ClearCollections = () => {
